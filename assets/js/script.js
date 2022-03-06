5d17ea2bf6d7268b4c312d1611e7c573e391807a
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-03-05 20:07:33 -0600

    fixed website crash

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,7 +141,7 @@ var taskType = taskSelected.querySelector("span.task-type").textContent;
 document.querySelector("input[name='task-name']").value = taskName;
 document.querySelector("select[name='task-type']").value = taskType;
 document.querySelector("#save-task").textContent = "Save Task";
-formEl.setAttribute("data-task-id", taskId);
+
 };
 
 formEl.addEventListener("submit", taskFormHandler);
